312de9b7e485a5fe6015f107d54a49b689e9a544
Luis Uicab <uicab2593@gmail.com>
2026-01-15 23:53:17 -0500

    ads

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,7 +1,9 @@
 import { defineStore } from '#q-app/wrappers'
 import { createPinia } from 'pinia'
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-
+// import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import { createPersistedStatePlugin } from 'pinia-plugin-persistedstate-2'
+// import persistedState from 'pinia-plugin-persistedstate-2'
+import localforage from 'localforage'
 /*
  * If not building with SSR mode, you can
  * directly export the Store instantiation;
@@ -13,10 +15,17 @@ import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 export default defineStore((/* { ssrContext } */) => {
   const pinia = createPinia()
-  pinia.use(piniaPluginPersistedstate)
+  // pinia.use(piniaPluginPersistedstate)
+  const persistedState = createPersistedStatePlugin({
+    storage: {
+      getItem: async key => await localforage.getItem(key),
+      setItem: async (key, value) => await localforage.setItem(key, value),
+      removeItem: async key => await localforage.removeItem(key),
+    }
+  })
 
   // You can add Pinia plugins here
-  // pinia.use(SomePiniaPlugin)
+  pinia.use(persistedState)
 
   return pinia
 })
--- src/stores/quote.js
+++ src/stores/quote.js
@@ -1,31 +1,35 @@
 import { defineStore } from 'pinia'
-import { ref, computed, watch } from 'vue'
-// import { i18n } from 'vue-i18n';
+import { ref } from 'vue'
 
-export const useQuoteStore = defineStore('quotes', () => {
+export const useQuoteStore = defineStore(
+  'quotes',
+  () => {
+    const quoteConsecutive = ref(200)
+    const txts = ref([
+      { value: 'Unidad jac sunray para 14 personas, a/c, unidades de carga usb, maletero, asientos reclinables, WiFi' },
+      { value: 'Unidad Toyota hiace para 11 personas, a/c, maletero, canasta, asientos reclinables, WiFi' },
+      { value: 'Unidad maxus para 11 personas, a/c, maletero, unidades de carga usb, maletero, asientos reclinables, WiFi' },
+    ])
 
-  const quoteConsecutive = ref(200)
-  const txts = ref([
-    {value:'Unidad  jac sunray para 14 personas, a/c, unidades de carga usb, maletero, asientos reclinables, WiFi'},
-    {value:'Unidad Toyota hiace para 11 personas, a/c, maletero, canasta, asientos reclinables, WiFi'},
-    {value:'Unidad maxus para 11 personas, a/c, maletero, unidades de carga usb, maletero, asientos reclinables, WiFi'},
-  ])
+    const lastmsg = ref(
+      'TODO MOVIMIENTO EXTRA A LO COTIZADO TIENE COSTO ADICIONAL. EL SERVICIO INCLUYE INICAMENTE LOS KILOMETROS ESTABLECIDOS EN LA PRESENTE COTIZACION CUALQUIER RECORIDO ADICIONAL O EXTENCION DEL TRAYECTO TENDRA UN COSTO ADICIONAL'
+    )
 
-  const lastmsg = ref("TODO MOVIMIENTO EXTRA A LO COTIZADO TIENE COSTO ADICIONAL. EL SERVICIO INCLUYE INICAMENTE LOS KILOMETROS ESTABLECIDOS EN LA PRESENTE COTIZACION CUALQUIER RECORIDO ADICIONAL O EXTENCION DEL TRAYECTO TENDRA UN COSTO ADICIONAL")
-
-  return {
-    quoteConsecutive,
-    txts,
-    lastmsg,
+    return {
+      quoteConsecutive,
+      txts,
+      lastmsg,
+    }
+  },
+  {
+    persist: {
+      enabled: true,
+      strategies: [
+        {
+          key: 'quotes-store',
+          paths: ['quoteConsecutive', 'txts', 'lastmsg'],
+        },
+      ],
+    },
   }
-},{
-  persist: {
-    enabled:true,
-    strategies: [
-      {
-        storage: localStorage,
-        paths: ['quoteConsecutive','txts','lastmsg'],
-      },
-    ]
-  }
-})
+)
